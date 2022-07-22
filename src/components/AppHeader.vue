eda6403d7e2bfea8a778c734590cc38d0beb2604
zhangwuyang <849174463@qq.com>
2022-07-22 12:16:10 +0800

    feat:修改导航样式

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -32,11 +32,12 @@
           </div>
         </div>
         <div class="app-header-right">
-          <ul class="app-header-dots">
+          <ul class="app-header-dots" v-if="isLogin">
             <li>
               <router-link to="/upload">
-                <el-button type="primary" class="el-btn-upload">
+                <el-button class="el-btn-upload">
                   <span>上传</span>
+                  &nbsp;
                   <i class="el-icon-upload el-icon--right"></i>
                 </el-button>
               </router-link>
@@ -57,7 +58,7 @@
             </li>
 
             <li>
-              <el-dropdown>
+              <el-dropdown class="el-dropdown-userinfo">
                 <el-button style="background: #f5f5f5; border: none">
                   <span style="display: flex; align-items: center">
                     <i
@@ -67,14 +68,44 @@
                     <i class="el-icon-arrow-down el-icon--right"></i>
                   </span>
                 </el-button>
-                <el-dropdown-menu slot="dropdown">
-                  <el-dropdown-item>xxx</el-dropdown-item>
+                <el-dropdown-menu
+                  slot="dropdown"
+                  class="el-dropdown-menu-userinfo"
+                >
+                  <el-dropdown-item class="header-userinfo">
+                    <div class="header-avatar">
+                      <img
+                        src="http://dummyimage.com/40x40/ef79f2/FFF&text=1"
+                      />
+                    </div>
+                    <div class="username-and-email">
+                      <span class="username">xxxxxxxxxxxxxx</span>
+                      <span class="email">51222212111@qq.com</span>
+                    </div>
+                  </el-dropdown-item>
                   <el-dropdown-item>概况</el-dropdown-item>
                   <el-dropdown-item>历史</el-dropdown-item>
                   <el-dropdown-item>设置</el-dropdown-item>
                   <el-dropdown-item>退出登录</el-dropdown-item>
                 </el-dropdown-menu>
               </el-dropdown>
+            </li>
+          </ul>
+
+          <ul class="app-header-nologin" v-else>
+            <li>
+              <el-button
+                style="background: #f5f5f5; border: none; font-size: 20px"
+              >
+                Log in
+              </el-button>
+            </li>
+            <li>
+              <el-button
+                style="background: #f5f5f5; border: none; font-size: 20px"
+              >
+                Sign up
+              </el-button>
             </li>
           </ul>
         </div>
@@ -87,6 +118,7 @@
 export default {
   data() {
     return {
+      isLogin: true,
       activeIndex: "1",
       activeIndex2: "1",
       keywords: "",
@@ -174,6 +206,17 @@ export default {
         justify-content: space-between;
 
         .el-btn-upload {
+          width: 100px;
+          background: #1e78f0;
+          color: #fff;
+        }
+      }
+      ul.app-header-nologin {
+        list-style-type: none;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        .el-btn-upload {
           width: 90px;
           background: #88bbf3;
         }
@@ -186,5 +229,59 @@ export default {
   box-shadow: 0 0.46875rem 2.1875rem rgb(4 9 20 / 3%),
     0 0.9375rem 1.40625rem rgb(4 9 20 / 3%),
     0 0.25rem 0.53125rem rgb(4 9 20 / 5%), 0 0.125rem 0.1875rem rgb(4 9 20 / 3%);
+}
+.el-dropdown-menu-userinfo {
+  li {
+    height: 50px;
+    line-height: 50px;
+    width: 95%;
+    margin: 0 auto;
+    border-radius: 8px;
+  }
+  li:hover {
+    background: #8ab5ef;
+    color: #f5f5f5;
+  }
+  .header-userinfo {
+    width: 100%;
+    height: 58px;
+    display: flex;
+    align-items: center;
+    .header-avatar img {
+      border-radius: 50%;
+      overflow: hidden;
+      vertical-align: middle;
+    }
+    .username-and-email {
+      margin-left: 10px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      height: 100%;
+      justify-content: space-around;
+      .username {
+        font-weight: 500;
+        font-size: 16px;
+        color: #333;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+        width: 100%;
+        line-height: 16px;
+      }
+      .email {
+        font-size: 12px;
+        line-height: 12px;
+        color: #aaa;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+        width: 100%;
+      }
+    }
+  }
+  .header-userinfo:hover {
+    background: unset !important;
+  }
 }
 </style>
