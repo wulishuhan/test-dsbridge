3a4b2015fa4ebc3f14e6940068256165d0164ae8
shuhan <438115215@qq.com>
2022-09-21 16:44:21 +0800

    fix: iosScale

--- src/utils/iosScale.js
+++ src/utils/iosScale.js
@@ -1,12 +1,22 @@
 window.onload = function () {
   document.addEventListener("touchstart", function (event) {
     if (event.touches.length > 1) {
-      console.log("test1");
       event.preventDefault();
     }
   });
+  var lastTouchEnd = 0;
+  document.addEventListener(
+    "touchend",
+    function (event) {
+      var now = new Date().getTime();
+      if (now - lastTouchEnd <= 300) {
+        event.preventDefault();
+      }
+      lastTouchEnd = now;
+    },
+    false
+  );
   document.addEventListener("gesturestart", function (event) {
-    console.log("test2");
     event.preventDefault();
   });
 };
