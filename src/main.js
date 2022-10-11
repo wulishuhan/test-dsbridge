9a546c4326f4020f974bdebadd5b1a588e898e02
zhangwuyang <849174463@qq.com>
2022-10-11 10:08:14 +0800

    feat:引入vue样式

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
+/* eslint-disable */
 import i18n from "@/utils/i18n";
-import Vue from "vue";
+// import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
